865d288661999dad8c6f4b805e905c79368fbf2b
Puneet Sharma <yoiampuneet@gmail.com>
2024-02-03 02:34:50 +0530

    CampingProject

---
+++ public/js/clusterMap.js
@@ -0,0 +1,26 @@
+let a = JSON.parse(camps);
+let markers = [];
+a.forEach((element) => {
+  let latlng = {};
+  latlng.lat = element.geometry.coordinates[0];
+  latlng.lng = element.geometry.coordinates[1];
+  markers.push(latlng);
+});
+
+const map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+
+const markersClusterGroup = L.markerClusterGroup();
+
+markers.forEach((markerData) => {
+  const marker = L.marker([markerData.lat, markerData.lng]);
+  marker.bindPopup(`<b>Iilam</b><br>Most visited place`); // Move this line inside the loop
+  markersClusterGroup.addLayer(marker); // Add the marker to the cluster group
+});
+
+map.addLayer(markersClusterGroup);
+map.fitBounds(markersClusterGroup.getBounds());
+markersClusterGroup.getLayers()[0].openPopup();
